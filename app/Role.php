a6abc3cc3ea73a703e29ad0fc7b05ea94f84a329
abdulrafey38 <abdulrafey38@gmail.com>
2020-07-29 13:57:04 +0500

    Working Till here

--- app/Role.php
+++ app/Role.php
@@ -8,6 +8,7 @@ class Role extends Model
 {
     protected $fillable = [
         'name'
+    
     ];
     //
 }
--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -20,8 +20,8 @@
         {!! Form::password('password' , ['class' => 'form-control']) !!}
     </div>
     <div class="form-group">
-        {!! Form::label('status','Status:') !!}
-        {!! Form::select('status',array(1=>'Active',0=>'Not Active'), 0 , ['class' => 'form-control']) !!}
+        {!! Form::label('is_active','Status:') !!}
+        {!! Form::select('is_active',array(1=>'Active',0=>'Not Active'), 0 , ['class' => 'form-control']) !!}
     </div>
     <div class="form-group">
         {!! Form::label('role_id','Role:') !!}
@@ -33,7 +33,7 @@
     </div>
 
     <div class="form-group">
-        {!! Form::submit('Craete Post', ['class'=>'btn btn-primary']) !!}
+        {!! Form::submit('Create Post', ['class'=>'btn btn-primary']) !!}
     </div>
     {!! Form::close() !!}
 
